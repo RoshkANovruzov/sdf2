3bcc0ee21749610db52a1c3803fcd591d61be678
RashadNovruzov <r4shad.novruz0v@gmail.com>
2023-12-16 23:42:37 +0400

    added files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,7 @@
 {
     partial class frmRegister
     {
-        /// <summary>
-        /// Variável de designer necessária.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Limpar os recursos que estão sendo usados.
-        /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +12,6 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
-
-        /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
@@ -235,8 +221,6 @@
             this.PerformLayout();
 
         }
-
-        #endregion
 
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
